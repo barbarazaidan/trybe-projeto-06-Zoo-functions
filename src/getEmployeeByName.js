7d47076f5265e35d58554ebd3e9d0fae21572aa7
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-10-31 17:33:55 -0300

    finaliza requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+// console.log(data); - retorna um grande objeto
+
+// find
+const empregados = data.employees; // retorna um array de objetos
+// console.log(empregados);
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return empregados
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);// eu desestruturei o objeto 'empregado' para pegar apenas as suas propriedades first e last name. O objetivo foi encurtar a frase para que ela tivesse menos de 100 caracteres de comprimento, pois o lint barra quando ela tem mais do que isso. Por conta do lint, também coloquei o find() para baixo, de modo a diminuir o tamanho da linha. Tentei fazer com return normal, mas por ele ser de apenas uma linha, o lint também não permite eu descer o corpo da função.
 }
+
+// console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -13,5 +13,6 @@ function getSpeciesByIds(...ids) {
 }
 
 // console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+// na saída aparece o residents: [ [object], [object], [object] ]. Isso ocorre apenas na saída do console.log, para encurtar as informações, porém o objeto inteiro continua lá e vai passar nos testes.
 
 module.exports = getSpeciesByIds;
